42070af71b7d7a1b58b7be2ff1ee60370cd97bc4
Anurag Srivastava <anuragf45@gmail.com>
2022-09-17 13:30:14 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,4 +8,8 @@ router.post('/functionup/interns',internController.createIntern);
 router.get('/functionup/collegeDetails',collegeController.getCollege)
 
 
+router.all("/*", function (req, res) {
+    res.status(400).send({ status: false, message: "Invalid path params" });
+  });
+
 module.exports = router;
